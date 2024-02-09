da0fc560ef5b5cecd8f41911649afb419b1e0b58
Christian Jeune <145855247+Christian-Jeune@users.noreply.github.com>
2024-02-09 21:18:08 +0000

    Update Stack

--- Tutorial 4/Stack.h
+++ Tutorial 4/Stack.h
@@ -1,1 +1,16 @@
+#ifndef STACK_H_
+#define STACK_H_
+class Stack {
+public:
+    Stack();
+    void push(int item);
+    void pop();
+    int peek();
+    bool isEmpty();
+    bool contains(int item);
+};
 
+
+
+
+#endif
